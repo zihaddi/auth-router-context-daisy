df9f2f270c710ed7121d23bfe1b14fb446b1dded
zihad <zihad.jamil2018@gmail.com>
2022-10-19 04:28:48 +0600

    adding router setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
+import Main from './layouts/Main';
 
 function App() {
+  const router  = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/registration',
+          element:<Registration></Registration>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <div>
+      <h3>This is registration</h3>
+    </div>
+  );
+};
+
+export default Registration;
