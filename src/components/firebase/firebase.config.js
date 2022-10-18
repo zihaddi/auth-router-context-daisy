4f119a9167136e7f984a3ad183c4ec5e3f2365e0
zihad <zihad.jamil2018@gmail.com>
2022-10-19 04:17:22 +0600

    adding firebase config file

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyxoGt84fpXsJ6d-tUPNbw2VEZVaAzsiE",
+  authDomain: "auth-router-context-dais-df445.firebaseapp.com",
+  projectId: "auth-router-context-dais-df445",
+  storageBucket: "auth-router-context-dais-df445.appspot.com",
+  messagingSenderId: "616506728828",
+  appId: "1:616506728828:web:14cf1f0e5c487a0c7cc4f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
